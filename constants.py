ac9f4da0e066ce207915b12bc3769cf522d6b610
Shamik Bera <berashamik115@gmail.com>
2025-09-15 20:21:02 +0530

    chore: created a mapper for providers and their supported models;

---
+++ constants.py
@@ -0,0 +1,31 @@
+supported_models = {
+    "openai": [
+        "gpt-4.1",
+        "gpt-4.1-mini",
+        "gpt-4.1-nano",
+        "o1",
+        "o1-mini",
+        "o1-pro",
+        "o3",
+        "o3-mini",
+        "o3-pro",
+        "o4-mini",
+        "gpt-4o",
+        "gpt-4o-mini",
+        # deprecated / preview models (may be removed or replaced)
+        "gpt-4.5",
+        "gpt-4o-mini-preview",
+    ],
+    "groq": [
+        "llama-3.1-8b-instant",
+        "llama-3.3-70b-versatile",
+        "meta-llama/llama-guard-4-12b",
+        "openai/gpt-oss-120b",
+        "openai/gpt-oss-20b",
+        "meta-llama/llama-4-maverick-17b-128e-instruct",
+        "meta-llama/llama-4-scout-17b-16e-instruct",
+        "moonshotai/kimi-k2-instruct-0905",
+        "qwen/qwen3-32b",
+        "deepseek-r1-distill-llama-70b",  # sometimes called “DeepSeek R1 Distill Llama 70B”
+    ],
+}
